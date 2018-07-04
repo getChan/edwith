73546aede894fd55e89128ea860ac401ae390932
getChan <9511chn@gmail.com>
2018-07-04 19:56:13 +0900

    Finish pagerank

---
+++ python/src/pagerank/spider.js
@@ -0,0 +1,16 @@
+spiderJson = {"nodes":[
+{"weight":4,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":5.469696969696958, "id":3, "url":"http://www.dr-chuck.com/office"},
+{"weight":1,"rank":0.0, "id":4, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":2,"rank":10.795454545454541, "id":6, "url":"http://www.dr-chuck.com/dr-chuck/resume/bio.htm"},
+{"weight":1,"rank":4.462121212121211, "id":11, "url":"http://www.dr-chuck.com/dr-chuck/resume/leadership.htm"},
+{"weight":2,"rank":9.212121212121211, "id":12, "url":"http://www.dr-chuck.com/dr-chuck/resume/speaking.htm"}],
+"links":[
+{"source":0,"target":3,"value":3},
+{"source":0,"target":4,"value":3},
+{"source":0,"target":5,"value":3},
+{"source":5,"target":0,"value":3},
+{"source":5,"target":3,"value":3},
+{"source":3,"target":0,"value":3},
+{"source":3,"target":5,"value":3},
+{"source":4,"target":0,"value":3}]};
